87d849691c37ac6da5b72676b1bdf201ef2534a1
paunstefan <paunstefan@protonmail.com>
2022-10-07 21:18:44 +0300

    Writing to flash prototype

--- src/bin/flash.rs
+++ src/bin/flash.rs
@@ -1,6 +1,9 @@
 #![no_std]
 #![no_main]
 
+use core::fmt::Write;
+
+use hal::rom_data::flash_enter_cmd_xip;
 // The macro for our start-up function
 use rp_pico::entry;
 
@@ -18,15 +21,30 @@ use rp_pico::hal::pac;
 // A shorter alias for the Hardware Abstraction Layer, which provides
 // higher-level drivers.
 use rp_pico::hal;
+use rp_pico::hal::rom_data::{
+    connect_internal_flash, flash_exit_xip, flash_flush_cache, flash_range_erase,
+    flash_range_program,
+};
 
 // Flash constants
 const FLASH_START: usize = 0x10000000;
 const FLASH_SIZE: usize = 2048 * 1024;
 const FLASH_SECTOR_SIZE: usize = 4096;
 const FLASH_PAGE_SIZE: usize = 256;
-const FLASH_OFFSET: usize = (FLASH_SIZE - FLASH_SECTOR_SIZE);
+const FLASH_BLOCK_SIZE: u32 = 65536;
+const FLASH_OFFSET: usize = FLASH_SIZE - FLASH_SECTOR_SIZE;
 const FLASH_WR: *mut u8 = (FLASH_START + FLASH_OFFSET) as *mut u8;
 
+static mut UART: Option<
+    hal::uart::UartPeripheral<
+        hal::uart::Enabled,
+        pac::UART0,
+        (
+            hal::gpio::Pin<hal::gpio::bank0::Gpio16, hal::gpio::Function<hal::gpio::Uart>>,
+            hal::gpio::Pin<hal::gpio::bank0::Gpio17, hal::gpio::Function<hal::gpio::Uart>>,
+        ),
+    >,
+> = None;
 /// Entry point to our bare-metal application.
 ///
 /// The `#[entry]` macro ensures the Cortex-M start-up code calls this function
@@ -77,46 +95,165 @@ fn main() -> ! {
         pins.gpio17.into_mode::<hal::gpio::FunctionUart>(),
     );
 
-    let uart = hal::uart::UartPeripheral::new(pac.UART0, uart_pins, &mut pac.RESETS)
+    let mut uart = hal::uart::UartPeripheral::new(pac.UART0, uart_pins, &mut pac.RESETS)
         .enable(
             hal::uart::common_configs::_9600_8_N_1,
             hal::Clock::freq(&clocks.peripheral_clock),
         )
         .unwrap();
     uart.write_full_blocking(b"UART works\n\r");
+    unsafe {
+        UART = Some(uart);
+    }
 
     // write to flash
     let mut buf = [0u8; 256];
     buf[0] = 0xCA;
     buf[1] = 0xFE;
 
-    uart.write_full_blocking(b"a");
-
     cortex_m::interrupt::free(|_| unsafe {
-        hal::rom_data::flash_range_erase(FLASH_OFFSET as u32, 4096, 4096, 0xd8);
-        uart.write_full_blocking(b"b");
-        hal::rom_data::flash_range_program(FLASH_OFFSET as u32, buf.as_ptr(), 256);
+        flash_range_erase_safe(FLASH_OFFSET as u32, 4096);
+        unsafe {
+            UART.as_mut().unwrap().write_full_blocking(b"b\n\r");
+        }
+        flash_range_program_safe(FLASH_OFFSET as u32, &buf, 256);
     });
-
-    //uart.write_full_blocking(b"written data\r\n");
 
     // read flash
     let mut a = [0xAB, 0xCD];
-    uart.write_full_blocking(&a[0..1]);
-    uart.write_full_blocking(&a[1..2]);
+    unsafe {
+        UART.as_mut()
+            .unwrap()
+            .write_fmt(format_args!("intial: {:x} {:x}\n\r", a[0], a[1]))
+            .unwrap();
+    }
 
     cortex_m::interrupt::free(|_| unsafe {
         a[0] = core::ptr::read_volatile(FLASH_WR);
         a[1] = core::ptr::read_volatile(FLASH_WR.offset(1));
     });
 
-    uart.write_full_blocking(&a[0..1]);
-    uart.write_full_blocking(&a[1..2]);
+    unsafe {
+        UART.as_mut()
+            .unwrap()
+            .write_fmt(format_args!("after: {:x} {:x}\n\r", a[0], a[1]))
+            .unwrap();
+    }
 
     if a[0] == 0xCA && a[1] == 0xFE {
         led_pin.set_high().unwrap();
     }
 
+    // new method
+    buf[0] = 0xBE;
+    buf[1] = 0xEF;
+    write_flash(FLASH_OFFSET as u32, &buf);
+
+    let r = read_flash(FLASH_OFFSET, 2);
+    unsafe {
+        UART.as_mut()
+            .unwrap()
+            .write_fmt(format_args!("new method: {:x} {:x}\n\r", r[0], r[1]))
+            .unwrap();
+    }
+
     // Run forever, setting the LED according to the button
     loop {}
 }
+
+const SECTOR_ERASE: u8 = 0x20; // Tested and works with W25Q16JV flash chip
+const BLOCK32_ERASE: u8 = 0x52;
+const BLOCK64_ERASE: u8 = 0xD8;
+
+#[inline(never)]
+#[link_section = ".data.ram_code"]
+fn write_flash(addr: u32, data: &[u8]) {
+    let size = data.len();
+    assert!(addr as usize + size <= FLASH_SIZE);
+    assert!(addr as usize & (FLASH_SECTOR_SIZE - 1) == 0);
+    assert!(size & (FLASH_PAGE_SIZE - 1) == 0);
+
+    cortex_m::interrupt::free(|_| unsafe {
+        connect_internal_flash();
+        flash_exit_xip();
+
+        flash_range_erase(addr, FLASH_SECTOR_SIZE, FLASH_BLOCK_SIZE, SECTOR_ERASE);
+        flash_range_program(addr, data.as_ptr(), size);
+
+        flash_flush_cache();
+        flash_enter_cmd_xip();
+    });
+}
+
+fn read_flash(addr: usize, size: usize) -> &'static [u8] {
+    let addr = (FLASH_START + addr) as *mut u8;
+    let data = cortex_m::interrupt::free(|_| unsafe { core::slice::from_raw_parts(addr, size) });
+
+    data
+}
+
+#[inline(never)]
+#[link_section = ".data.ram_code"]
+fn flash_range_erase_safe(addr: u32, size: usize) {
+    assert!(addr as usize + size <= FLASH_SIZE);
+    assert!(addr as usize & (FLASH_SECTOR_SIZE - 1) == 0);
+    assert!(size & (FLASH_SECTOR_SIZE - 1) == 0);
+
+    unsafe {
+        connect_internal_flash();
+        flash_exit_xip();
+
+        flash_range_erase(addr, size, FLASH_BLOCK_SIZE, SECTOR_ERASE);
+
+        flash_flush_cache();
+        flash_enter_cmd_xip();
+    }
+}
+
+#[inline(never)]
+#[link_section = ".data.ram_code"]
+fn flash_range_program_safe(addr: u32, data: &[u8], count: usize) {
+    assert!(addr as usize + count <= FLASH_SIZE);
+    assert!(addr as usize & (FLASH_PAGE_SIZE - 1) == 0);
+    assert!(count & (FLASH_PAGE_SIZE - 1) == 0);
+
+    unsafe {
+        connect_internal_flash();
+        flash_exit_xip();
+
+        flash_range_program(addr, data.as_ptr(), count);
+
+        flash_flush_cache();
+        flash_enter_cmd_xip();
+    }
+}
+
+// #[inline(never)]
+// #[link_section = ".data.ram_code"]
+// fn flash_init_boot2_copyout() {
+//     if unsafe { BOOT2_COPYOUT_VALID } {
+//         return;
+//     }
+
+//     let XIP_BASE = FLASH_START as *mut u32;
+
+//     for i in 0..BOOT_SIZE_WORDS {
+//         unsafe {
+//             BOOT2_COPYOUT[i] = core::ptr::read(XIP_BASE.add(i));
+//         }
+//     }
+//     atomic::compiler_fence(atomic::Ordering::SeqCst);
+//     unsafe {
+//         BOOT2_COPYOUT_VALID = true;
+//     }
+// }
+
+// #[inline(never)]
+// #[link_section = ".data.ram_code"]
+// fn flash_enable_xip_via_boot2() {
+//     unsafe {
+//         let ptr = BOOT2_COPYOUT.as_ptr().add(1) as *const ();
+//         let code: extern "C" fn() = core::mem::transmute(ptr);
+//         (code)();
+//     }
+// }
